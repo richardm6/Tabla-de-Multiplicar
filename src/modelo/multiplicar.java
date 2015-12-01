d4d328a1f6cabc9230effe06fe44f687eeae0a90
richardm6 <vinicio_m6@hotmail.com>
2015-12-01 01:10:10 -0500

    mi primer commit

---
+++ src/modelo/multiplicar.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author RICARDO
+ */
+public class multiplicar {
+    public int multiplicando=0;
+    
+    public int[] Calcular (){
+       int[] vec=new int [12];
+       
+       for (int i=0; i<vec.length;i++){
+           vec[i]=(i+1)*multiplicando;
+       }
+       
+       
+       
+       
+       return vec;
+    }
+    
+}
